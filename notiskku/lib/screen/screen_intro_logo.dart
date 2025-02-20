54c974c4eb52fed7bc52943173d9d159983aa76a
twjin03 <twjin03@g.skku.edu>
2025-02-21 02:36:59 +0900

    add flutter_riverpod

---
+++ notiskku/lib/screen/screen_intro_logo.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ScreenLogoIntro extends StatefulWidget {
+  const ScreenLogoIntro({super.key});
+
+  @override
+  State<ScreenLogoIntro> createState() => _ScreenLogoIntroState(); 
+}
+
+class _ScreenLogoIntroState extends State<ScreenLogoIntro> {
+  @override
+  void initState() {
+    super.initState();
+    // 일정 시간 후에 IntroductionScreen으로 이동
+    Future.delayed(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        // MaterialPageRoute(builder: (context) => const IntroductionScreen()),
+        MaterialPageRoute(builder: (context) => const ScreenIntroOne()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Get screen dimensions
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    
+    return Scaffold(
+      backgroundColor: const Color(0xff0b5b42),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              //'assets/images/logo.png',
+              'assets/images/whitelogo_fix.png',
+              width: screenWidth * 0.8, // Set width as 80% of screen width
+              height: screenHeight * 0.25, // Set height as 25% of screen height
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 10), // 간격 추가
+
+            Text(
+              'NotiSKKU',
+              style: TextStyle(
+                fontSize: screenWidth * 0.15, // Font size as 15% of screen width
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
