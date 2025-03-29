873072990effa8b6566c70dd5862707e923640d2
i-chaeyeon <leechae0605@naver.com>
2025-03-29 23:12:19 +0900

    feat: Add google sheets api

--- notiskku/lib/google/google_sheets_api.dart
+++ notiskku/lib/google/google_sheets_api.dart
@@ -1,1 +1,105 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:googleapis/sheets/v4.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:notiskku/models/notice.dart';
+import 'package:notiskku/services/preference_services.dart';
 
+class GoogleSheetsAPI {
+  static const _commonSheetId = "1RPTHVpyEJb4mZs9sz10E5OwIpZB-3YJcfrg5H7dFqhM";
+  static const _deptSheetId = "1YX53hQ1oHjAhl8wvsa6nIq6P5Eh0SMyV8SaN8hsPyTg";
+  static const _majorSheetId = "1XOv61iZQiPwc2CeqU4lnZsnmpUIhp4Tnjcsk84mHihU";
+
+  /// Google Sheets API 연결 메소드
+  /// _getSheetsApi
+  static Future<SheetsApi?> _getSheetsApi() async {
+    final credentials = await rootBundle.loadString('assets/credentials.json');
+    final serviceAccountCredentials = ServiceAccountCredentials.fromJson(
+      json.decode(credentials),
+    );
+
+    final client = await clientViaServiceAccount(serviceAccountCredentials, [
+      SheetsApi.spreadsheetsScope,
+    ]);
+    return SheetsApi(client);
+  }
+
+  /// 전체 공지 시트 데이터를 Notice 리스트로 변환해서 리턴
+  /// readCommonData(startRow, limit)
+  static Future<List<Notice>> readCommonData({
+    int startRow = 1,
+    int limit = 10,
+  }) async {
+    final sheetsApi = await _getSheetsApi();
+    if (sheetsApi == null) return [];
+
+    final response = await sheetsApi.spreadsheets.values.get(
+      _commonSheetId,
+      "시트1",
+    );
+
+    final allData = response.values ?? [];
+
+    return allData.isNotEmpty
+        ? allData.reversed
+            .skip(startRow - 1)
+            .take(limit)
+            .map((row) => Notice.fromSheet(row.cast<String>()))
+            .toList()
+        : [];
+  }
+
+  /// 단과대별 공지 시트 데이터를 Notice 리스트로 변환해서 리턴
+  /// readDeptData(majors, startRow, limit)
+  /// TODO: 전공 여러 개인 경우 어떻게 처리할 것인지
+  static Future<List<Notice>> readDeptData({
+    required List<String> majors,
+    int startRow = 1,
+    int limit = 10,
+  }) async {
+    final sheetsApi = await _getSheetsApi();
+    if (sheetsApi == null) return [];
+
+    final response = await sheetsApi.spreadsheets.values.get(
+      _deptSheetId,
+      "시트1",
+    );
+
+    final allData = response.values ?? [];
+
+    return allData.isNotEmpty
+        ? allData.reversed
+            .skip(startRow - 1)
+            .take(limit)
+            .map((row) => Notice.fromSheet(row.cast<String>()))
+            .toList()
+        : [];
+  }
+
+  /// 학과별 공지 시트 데이터를 Notice 리스트로 변환해서 리턴
+  /// readMajorData(majors, startRow, limit)
+  /// TODO: 전공 여러 개인 경우 어떻게 처리할 것인지
+  static Future<List<Notice>> readMajorData({
+    required List<String> majors,
+    int startRow = 1,
+    int limit = 10,
+  }) async {
+    final sheetsApi = await _getSheetsApi();
+    if (sheetsApi == null) return [];
+
+    final response = await sheetsApi.spreadsheets.values.get(
+      _majorSheetId,
+      "시트1",
+    );
+
+    final allData = response.values ?? [];
+
+    return allData.isNotEmpty
+        ? allData.reversed
+            .skip(startRow - 1)
+            .take(limit)
+            .map((row) => Notice.fromSheet(row.cast<String>()))
+            .toList()
+        : [];
+  }
+}
--- notiskku/lib/models/notice.dart
+++ notiskku/lib/models/notice.dart
@@ -1,33 +1,31 @@
-import 'dart:convert';
-
 class Notice {
+  final String id;
+  final String category;
   final String title;
-  final String url;
   final String date;
+  final String uploader;
   final String views;
+  final String link;
 
   Notice({
+    required this.id,
+    required this.category,
     required this.title,
-    required this.url,
     required this.date,
+    required this.uploader,
     required this.views,
+    required this.link,
   });
 
-  // JSON으로 변환
-  Map<String, dynamic> toJson() => {
-        'title': title,
-        'url': url,
-        'date': date,
-        'views': views,
-      };
-
-  // JSON에서 Notice 객체로 변환
-  factory Notice.fromJson(Map<String, dynamic> json) {
+  factory Notice.fromSheet(List<String> row) {
     return Notice(
-      title: json['title'],
-      url: json['url'],
-      date: json['date'],
-      views: json['views'],
+      id: row.isNotEmpty ? row[0] : "",
+      category: row.length > 1 ? row[1] : "",
+      title: row.length > 2 ? row[2] : "",
+      date: row.length > 3 ? row[3] : "",
+      uploader: row.length > 4 ? row[4] : "",
+      views: row.length > 5 ? row[5] : "",
+      link: row.length > 6 ? row[6] : "",
     );
   }
 }
