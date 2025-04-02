92d36bd996138bda8d85564fef733c14a310fea2
i-chaeyeon <leechae0605@naver.com>
2025-04-02 13:06:06 +0900

    docs: delete comments

--- notiskku/lib/providers/bar_providers.dart
+++ notiskku/lib/providers/bar_providers.dart
@@ -5,8 +5,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // 학과/키워드 탭 인덱스 관리 (0 = 학과, 1 = 키워드)
 final toggleIndexProvider = StateProvider<int>((ref) => 0);
 
-
-
 // BarNotices 관리
 // 학교 | 단과대학 | 학과
 final barNoticesProvider = StateProvider<int>((ref) => 0);
@@ -15,7 +13,6 @@ final barNoticesProvider = StateProvider<int>((ref) => 0);
 // 전체, 장학, 취업, 학사, …
 final barCategoriesProvider = StateProvider<int>((ref) => 0);
 
-
 // BarKeywords 관리
-// 공모전, 수강신청, 장학금, 계절학기 … 
+// 공모전, 수강신청, 장학금, 계절학기 …
 final barKeywordsProvider = StateProvider<int>((ref) => 0);
--- notiskku/lib/providers/list_notices_provider.dart
+++ notiskku/lib/providers/list_notices_provider.dart
@@ -8,45 +8,19 @@ import 'package:notiskku/providers/bar_providers.dart';
 import 'package:notiskku/providers/major_provider.dart';
 import 'package:notiskku/widget/bar/bar_categories.dart';
 
-final listNoticesProvider = StateNotifierProvider<
-  NoticesNotifier,
-  List<Notice>
->((ref) {
-  final categoryIndex = ref.watch(barCategoriesProvider);
-  final categoryName = BarCategories.categories[categoryIndex];
+class NoticeState {
+  final List<Notice> notices;
 
-  final majorState = ref.watch(majorProvider);
-  final selectedMajors = majorState.selectedMajors;
-  final barIndex = ref.watch(barNoticesProvider);
+  const NoticeState({this.notices = const []});
 
-  // 전공 선택 안했을 때 예외 처리 필요
-  Future<List<Notice>> fetchFunc({int startRow = 1, int limit = 10}) {
-    if (barIndex == 0) {
-      // 공통
-      return GoogleSheetsAPI.readCommonData(startRow: startRow, limit: limit);
-    } else if (barIndex == 1) {
-      // 단과대
-      return GoogleSheetsAPI.readDeptData(
-        dept: selectedMajors.first,
-        startRow: startRow,
-        limit: limit,
-      );
-    } else {
-      // 학과
-      return GoogleSheetsAPI.readMajorData(
-        major: selectedMajors.first,
-        startRow: startRow,
-        limit: limit,
-      );
-    }
+  NoticeState copyWith({List<Notice>? notices}) {
+    return NoticeState(notices: notices ?? this.notices);
   }
+}
 
-  return NoticesNotifier(categoryName: categoryName, fetchFunction: fetchFunc);
-});
-
-class NoticesNotifier extends StateNotifier<List<Notice>> {
+class NoticesNotifier extends StateNotifier<NoticeState> {
   NoticesNotifier({required this.fetchFunction, required this.categoryName})
-    : super([]) {
+    : super(const NoticeState()) {
     refreshNotices();
   }
 
@@ -61,7 +35,7 @@ class NoticesNotifier extends StateNotifier<List<Notice>> {
     hasMore = true;
     final allData = await fetchFunction(startRow: 1, limit: 10);
     final filtered = _filterByCategory(allData);
-    state = filtered;
+    state = state.copyWith(notices: filtered);
     currentRow += 10;
   }
 
@@ -73,7 +47,7 @@ class NoticesNotifier extends StateNotifier<List<Notice>> {
       hasMore = false;
     } else {
       final filtered = _filterByCategory(data);
-      state = [...state, ...filtered];
+      state = state.copyWith(notices: [...state.notices, ...filtered]);
       currentRow += 10;
     }
     isLoading = false;
@@ -84,3 +58,38 @@ class NoticesNotifier extends StateNotifier<List<Notice>> {
     return data.where((n) => n.category == "[$categoryName]").toList();
   }
 }
+
+final listNoticesProvider = StateNotifierProvider<NoticesNotifier, NoticeState>(
+  (ref) {
+    final categoryIndex = ref.watch(barCategoriesProvider);
+    final categoryName = BarCategories.categories[categoryIndex];
+
+    final majorState = ref.watch(majorProvider);
+    final selectedMajors = majorState.selectedMajors;
+
+    final barIndex = ref.watch(barNoticesProvider);
+
+    Future<List<Notice>> fetchFunc({int startRow = 1, int limit = 10}) {
+      if (barIndex == 0) {
+        return GoogleSheetsAPI.readCommonData(startRow: startRow, limit: limit);
+      } else if (barIndex == 1) {
+        return GoogleSheetsAPI.readDeptData(
+          dept: selectedMajors.first,
+          startRow: startRow,
+          limit: limit,
+        );
+      } else {
+        return GoogleSheetsAPI.readMajorData(
+          major: selectedMajors.first,
+          startRow: startRow,
+          limit: limit,
+        );
+      }
+    }
+
+    return NoticesNotifier(
+      categoryName: categoryName,
+      fetchFunction: fetchFunc,
+    );
+  },
+);
