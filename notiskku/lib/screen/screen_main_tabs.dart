87f6212fe71262ea1ff224744060756fdd1e691a
i-chaeyeon <leechae0605@naver.com>
2025-05-07 21:05:56 +0900

    fix: refresh issues within star functions

--- notiskku/lib/screen/screen_main_tabs.dart
+++ notiskku/lib/screen/screen_main_tabs.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notiskku/data/temp_starred_notices.dart';
+import 'package:notiskku/providers/user/user_provider.dart';
 import 'package:notiskku/tabs/screen_main_keyword.dart';
 import 'package:notiskku/tabs/screen_main_notice.dart';
 import 'package:notiskku/tabs/screen_main_box.dart';
@@ -23,17 +26,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ScreenMainTabs extends StatefulWidget {
+class ScreenMainTabs extends ConsumerStatefulWidget {
   const ScreenMainTabs({super.key});
 
   @override
-  State<ScreenMainTabs> createState() => _ScreenMainTabsState();
+  ConsumerState<ScreenMainTabs> createState() => _ScreenMainTabsState();
 }
 
-class _ScreenMainTabsState extends State<ScreenMainTabs> {
+class _ScreenMainTabsState extends ConsumerState<ScreenMainTabs> {
   int _selectedIndex = 0;
 
-  // 각 탭에 해당하는 페이지 리스트
   final List<Widget> _pages = const [
     ScreenMainNotice(),
     ScreenMainKeyword(),
@@ -42,7 +44,6 @@ class _ScreenMainTabsState extends State<ScreenMainTabs> {
     ScreenMainOthers(),
   ];
 
-  // 하단 네비게이션 아이템 정의
   List<BottomNavigationBarItem> get _navItems => [
     _buildNavItem('assets/images/notice_fix.png', '공지사항'),
     _buildNavItem('assets/images/keyword_fix.png', '키워드'),
@@ -51,7 +52,6 @@ class _ScreenMainTabsState extends State<ScreenMainTabs> {
     _buildNavItem('assets/images/more_fix.png', '더보기'),
   ];
 
-  // 아이템 구성 메서드
   BottomNavigationBarItem _buildNavItem(String assetPath, String label) {
     return BottomNavigationBarItem(
       icon: ImageIcon(AssetImage(assetPath), size: 30.w),
@@ -59,17 +59,19 @@ class _ScreenMainTabsState extends State<ScreenMainTabs> {
     );
   }
 
-  // 탭 선택 처리
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+
+    // ✅ ref 사용 가능
+    ref.read(userProvider.notifier).saveTempStarred(tempStarredNotices);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_selectedIndex], // 현재 선택된 페이지 표시
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         elevation: 0,
