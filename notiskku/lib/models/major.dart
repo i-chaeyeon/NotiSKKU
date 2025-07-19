3a31ddf95a3162c5c09382934c021b47c7a9dd68
twjin03 <twjindev@gmail.com>
2025-07-19 17:34:40 +0900

    fix: add 'id' field to major & keyword data

--- notiskku/lib/models/major.dart
+++ notiskku/lib/models/major.dart
@@ -1,9 +1,11 @@
 class Major {
+  final int id; 
   final String department;
   final String major;
   final bool receiveNotification;
 
   Major({
+    required this.id, 
     required this.department,
     required this.major,
     this.receiveNotification = false,
@@ -14,11 +16,13 @@ class Major {
       identical(this, other) ||
       other is Major &&
           runtimeType == other.runtimeType &&
-          department == other.department &&
-          major == other.major;
+          // department == other.department &&
+          // major == other.major;
+          id == other.id; 
 
   @override
-  int get hashCode => department.hashCode ^ major.hashCode;
+  // int get hashCode => department.hashCode ^ major.hashCode;
+  int get hashCode => id.hashCode; 
 
   Major copyWith({
     String? department,
@@ -26,6 +30,7 @@ class Major {
     bool? receiveNotification,
   }) {
     return Major(
+      id: id ?? this.id, 
       department: department ?? this.department,
       major: major ?? this.major,
       receiveNotification: receiveNotification ?? this.receiveNotification,
@@ -33,6 +38,7 @@ class Major {
   }
 
   Map<String, dynamic> toJson() => {
+    'id': id, 
     'department': department,
     'major': major,
     'receiveNotification': receiveNotification,
@@ -40,6 +46,7 @@ class Major {
 
   factory Major.fromJson(Map<String, dynamic> json) {
     return Major(
+      id: json['id'] ?? -1, 
       department: json['department'] ?? '',
       major: json['major'] ?? '',
       receiveNotification: json['receiveNotification'] ?? false,
