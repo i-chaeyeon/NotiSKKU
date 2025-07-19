b86893a5d2866cec843fa5de6a283c45e71b6a59
twjin03 <twjindev@gmail.com>
2025-07-19 17:39:21 +0900

    fix: change topic name field
    topic 설정 한글 미지원 이슈

--- notiskku/lib/firebase/topic_subscription.dart
+++ notiskku/lib/firebase/topic_subscription.dart
@@ -11,19 +11,15 @@ class TopicSubscription {
     // 전공 주제 구독 (전공, 단과대 각각에 대해 주제 구독)
     for (final m in majors) {
       if (m.receiveNotification) {
-        await FirebaseMessaging.instance.subscribeToTopic('major-${m.major}');
-        await FirebaseMessaging.instance.subscribeToTopic(
-          'department-${m.department}',
-        );
+        await FirebaseMessaging.instance.subscribeToTopic('major-${m.id}');
+        await FirebaseMessaging.instance.subscribeToTopic('department-${m.id}');
       }
     }
 
     // 키워드 주제 구독 'keyword-"선택한 키워드"' (예: "keyword-기숙사"와 같은 제목의 주제 구독)
     for (final k in keywords) {
       if (k.receiveNotification) {
-        await FirebaseMessaging.instance.subscribeToTopic(
-          'keyword-${k.keyword}',
-        );
+        await FirebaseMessaging.instance.subscribeToTopic('keyword-${k.id}');
       }
     }
   }
@@ -34,15 +30,11 @@ class TopicSubscription {
     required List<Major> majors,
   }) async {
     for (final m in majors) {
-      await FirebaseMessaging.instance.unsubscribeFromTopic('major-${m.major}');
-      await FirebaseMessaging.instance.unsubscribeFromTopic(
-        'department-${m.department}',
-      );
+      await FirebaseMessaging.instance.unsubscribeFromTopic('major-${m.id}');
+      await FirebaseMessaging.instance.unsubscribeFromTopic('department-${m.id}');
     }
     for (final k in keywords) {
-      await FirebaseMessaging.instance.unsubscribeFromTopic(
-        'keyword-${k.keyword}',
-      );
+      await FirebaseMessaging.instance.unsubscribeFromTopic('keyword-${k.id}');
     }
   }
 }
