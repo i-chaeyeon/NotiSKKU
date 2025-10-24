678bf941815139fec4fcb0e20059c46c5597fabc
twjin03 <twjindev@gmail.com>
2025-10-25 02:14:51 +0900

    feat: prevent screen rotation
    always keep portrait view

--- notiskku/lib/main.dart
+++ notiskku/lib/main.dart
@@ -1,34 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // 상태관리
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // 기기별 화면 크기 대응
+import 'package:flutter/services.dart'; // 화면 회전 제어용 패키지
+
+// Firebase 관련
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'firebase/firebase_options.dart';
 import 'firebase/notification_provider.dart';
-import 'firebase/background_handler.dart'; // ★ 추가
+import 'firebase/background_handler.dart';
+
 import 'package:notiskku/screen/screen_intro_logo.dart';
 
-final navigatorKey = GlobalKey<NavigatorState>();
+final navigatorKey = GlobalKey<NavigatorState>(); // 전역 navigator key
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase 초기화
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // ★ 백그라운드 핸들러는 top-level 함수로, main()에서 한 번만 등록
+  // 백그라운드 메시지 핸들러 등록
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-
-  // (권장) 알림 권한 요청
+  // 알림 권한 요청 (iOS / Android 13+)
   await FirebaseMessaging.instance.requestPermission(
     alert: true,
     badge: true,
     sound: true,
   );
-
   // 포그라운드/클릭 처리 등 앱 런타임 로직 초기화
   await NotificationProvider().init();
 
-  runApp(const ProviderScope(child: MyApp()));
+  // 세로 모드 고정 후 앱 실행
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    _,
+  ) {
+    runApp(const ProviderScope(child: MyApp()));
+  });
 }
 
 class MyApp extends StatelessWidget {
